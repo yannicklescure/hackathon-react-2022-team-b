8d57315995702a16fd6383d69856673c99b5847b
Francis <fnaud@lawebshop.ca>
2022-09-14 18:39:48 -0400

    ajout logo navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { COLORS } from "../constants";
+import logo from "../assets/space-y.png";
 
 const Navbar = () => {
   return (
     <Wrapper>
+        <StyledNavLink to="/"><Logo src={logo} /></StyledNavLink>
         <StyledNavLink to="/">Accueil</StyledNavLink>
-        <StyledNavLink to="/buy-a-ticket">Buy a Ticket</StyledNavLink>
-        <StyledNavLink to="/launch-history">Launch History</StyledNavLink>
-        <StyledNavLink to="/faq">FAQ</StyledNavLink>
-        <StyledNavLink to="/privacy-policy">Privacy Policy</StyledNavLink>
+        <StyledNavLink to="/buy-a-ticket">Acheter un billet</StyledNavLink>
+        <StyledNavLink to="/launch-history">Historique de lancement</StyledNavLink>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  position: relative;
   padding: 16px;
   background-color: ${COLORS.dark};
   color: ${COLORS.light};
@@ -27,6 +28,12 @@ const Wrapper = styled.div`
 const StyledNavLink = styled(NavLink)`
   color: ${COLORS.light};
   text-decoration: none;
+`;
+const Logo = styled.img`
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+  height: 25px;
 `;
 
 export default Navbar;
