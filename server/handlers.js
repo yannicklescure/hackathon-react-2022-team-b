e914544ca0b2aba83c29b3eca95b81d55292be1f
Étienne Gagnon <egagnon@lawebshop.ca>
2022-09-14 17:29:23 -0400

    tickets get action

--- server/handlers.js
+++ server/handlers.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const { MONGO_URI, DB_NAME } = process.env;
-const mcache = require('memory-cache');
 
 const option = {
   useNewUrlParser: true,
@@ -22,12 +21,21 @@ const createTicket = async (req, res) => {
 };
 
 const getTickets = async (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-
-  let data = {};
-
-  res.status(200).json({ status: 200, data, message: "success" });
+  const client = new MongoClient(MONGO_URI, option);
+  try {
+    await client.connect();
+    const db = client.db(DB_NAME);
+    const data = await db.collection("tickets").find().toArray();
+    console.log(data);
+    data
+      ? res.status(200).json({ status: 200, data, message: "success" })
+      : res.status(409).json({ status: 409, message: "ERROR" });
+  } catch (err) {
+    console.log("Error getting list of tickets", err);
+    res.status(500).json({ status: 500, message: err });
+  } finally {
+    client.close();
+  }
 };
 
 module.exports = {
