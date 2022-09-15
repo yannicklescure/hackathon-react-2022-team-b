d287aa6842d3a3834ba282b466f06d4268a92794
Francis <fnaud@lawebshop.ca>
2022-09-15 12:38:13 -0400

    prog billet PDF

--- client/src/pages/Complaint.jsx
+++ client/src/pages/Complaint.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
-import { COLORS } from "../constants";
 import ComplaintBoxes from "../components/ComplaintBoxes";
+import BasicDocument from "../components/PdfTicket";
 
 const Complaint = () => {
 
+    var sec = new Date().getSeconds();
+
     return (
-        <Wrapper>
-            <ComplaintBoxes />
-        </Wrapper>
+        <>
+            <BasicDocument
+                lastName='test'
+                email='email'
+                phone='phone'
+                address='adresse'
+                color='color'
+                price='106 506DC'
+                time={sec} />
+            <Wrapper>
+                <ComplaintBoxes />
+            </Wrapper>
+        </>
     );
 }
 
